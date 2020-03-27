83cee7e44fc1fc45117dc7dc253f3edff1290245
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-03-27 01:09:35 +0000

    fundamentalTask3

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -1,49 +1,88 @@
 // eslint-disable-next-line no-unused-vars
 const catalogue = [
-  "The Catcher in the Rye by J.D. Salinger (10)",
-  "Dracula by Bram Stoker (0)",
-  "Between the Assassinations by Aravind Adiga (9)",
-  "Wolf Hall by Hilary Mantel (33)",
-  "Bring Up The Bodies by Hilary Mantel (31)",
-  "A Place of Greater Safety by Hilary Mantel (11)",
-  "Giving Up the Ghost by Hilary Mantel (8)",
-  "The Assassination of Margaret Thatcher by Hilary Mantel (43)",
-  "The Yellow Wallpaper by Charlotte Perkins Gilman (12)",
-  "Conversations With Friends by Sally Rooney (1)",
-  "Normal People by Sally Rooney (2)",
-  "Everything I Never Told You by Celeste Ng (6)",
-  "2666 by Robert Bolaño (17)",
-  "By Night In Chile by Robert Bolaño (8)",
-  "A Tale of Two Cities by Charles Dickens (3)",
-  "Oliver Twist by Charles Dickens (7)",
-  "Great Expectations by Charles Dickens (1)",
-  "The Blind Assassin by Margaret Atwood (8)",
-  "Why Be Happy When You Could Be Normal? by Jeanette Winterson (19)",
-  "The Origin of Species by Charles Darwin (50)"
+  
+  {title: "The Catcher in the Rye", author: "J.D. Salinger", quantity: 10},
+  {title: "Dracula", author: "Bram Stoker",quantity: 0},
+  {title: "Between the Assassinations", author: "Aravind Adiga",quantity: 9},
+  {title: "Wolf Hall", author: "Hilary Mantel",quantity: 33},
+  {title: "Bring Up The Bodies", author: "Hilary Mantel",quantity: 31},
+  {title: "A Place of Greater Safety", author: "Hilary Mantel",quantity: 11},
+  {title: "Giving Up the Ghost", author: "Hilary Mantel",quantity: 8},
+  {title: "The Assassination of Margaret Thatcher", author: "Hilary Mantel",quantity: 43},
+  {title: "The Yellow Wallpaper", author: "Charlotte Perkins Gilman",quantity: 12},
+  {title: "Conversations With Friends", author: "Sally Rooney",quantity: 1},
+  {title: "Normal People", author: "Sally Rooney",quantity: 2},
+  {title: "Everything I Never Told You", author: "Celeste Ng",quantity: 6},
+  {title: "2666", author: "Robert Bolaño",quantity: 17},
+  {title: "By Night In Chile", author: "Robert Bolaño",quantity: 8},
+  {title: "A Tale of Two Cities", author: "Charles Dickens",quantity: 3},
+  {title: "Oliver Twist", author: "Charles Dickens",quantity: 7},
+  {title: "Great Expectations", author: "Charles Dickens",quantity: 1},
+  {title: "The Blind Assassin", author: "Margaret Atwood",quantity: 8},
+  {title: "Why Be Happy When You Could Be Normal?", author: "Jeanette Winterson",quantity: 19},
+  {title: "The Origin of Species", author: "Charles Darwin",quantity: 50},
 ];
+ 
 
-function countBooksByAuthor(author) {
+
+function countBooksByAuthor(authorName) {
   // Your code here
+  let list =0;
+for (let i=0;i<20;i++)
+{
+ if (catalogue[i].author === authorName ) {list++;}
+}
+ return list;
 }
 
 function checkBookByTitle(title) {
+let num=0;
   // Your code here
+  for (let i=0;i<20;i++)
+{
+ if (catalogue[i].title === title ) {num++}
+}
+if (num>=1){return true;}
+else return false;
 }
 
 function countBooksByFirstLetter(letter) {
   // Your code here
+let num =0;
+  for(let i=0; i<20;i++)
+  {
+if (catalogue[i].title.charAt(0) === letter.toUpperCase()) {num++;}
+  }
+  return num;
 }
 
 function getQuantity(title) {
   // Your code here
+  for (let i=0; i<20;i++)
+  {
+if (catalogue[i].title=== title) {return catalogue[i].quantity}
+  }
 }
 
 function getBooksByAuthor(author) {
   // Your code here
+  let list =[];
+  for (let i=0; i<20;i++)
+  {
+if (catalogue[i].author === author){ list.push(catalogue[i]);}
+  }
+  return list;
 }
 
 function checkQuantity(title, quantity) {
   // Your code here
+  let num =0;
+  for (let i=0;i<20;i++)
+  {
+    if (catalogue[i].title === title && catalogue[i].quantity >= quantity){ num++;}
+  }
+  if(num>0) {return true;}
+  else {return false;}
 }
 
 module.exports = {
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,5 +1,7 @@
 const catalogueService = require("../app/catalogue_service");
 
+
+
 describe("catalogueService", () => {
   describe("catalogueService.countBooksByAuthor", () => {
     test("returns the total number of books written by the given author", () => {
@@ -9,3 +11,45 @@ describe("catalogueService", () => {
     });
   });
 });
+
+describe("catalogueService.checkBookByTitle", () => {
+  test("returns true if the book exists", () => {
+    expect(catalogueService.checkBookByTitle("The Assassination of Margaret Thatcher")).toBe(true);
+    expect(catalogueService.checkBookByTitle("The Chronicles of Narnia")).toBe(false);
+  });
+});
+
+describe("catalogueService", () => {
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("return the total number of books that begin with this letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("w")).toBe(2);
+    });
+  });
+});
+
+describe("catalogueService", () => {
+  describe("catalogueService.getQuantity", () => {
+    test("return the quantity of this item which is in stock.", () => {
+      expect(catalogueService.getQuantity("A Place of Greater Safety")).toBe(11);
+    });
+  });
+});
+
+describe("catalogueService", () => {
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("return an array of books for an author", () => {
+      expect(catalogueService.getBooksByAuthor("Robert Bolaño")).toEqual([{ title: "2666", author: "Robert Bolaño", quantity: 17 },
+      { title: "By Night In Chile", author: "Robert Bolaño", quantity: 8 }]);
+    });
+  });
+});
+
+describe("catalogueService", () => {
+  describe("catalogueService.checkQuantity", () => {
+    test("return true if there are at least as many books in stock as the given quantity, and false otherwise", () => {
+      expect(catalogueService.checkQuantity("By Night In Chile", 4)).toBe(true);
+      expect(catalogueService.checkQuantity("By Night In Chile", 100)).toBe(false);
+    });
+  });
+});
+
